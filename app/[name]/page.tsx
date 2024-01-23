f9a4edf6258fa152d599f3859e1d8231f7924141
Oka Uliandana <oka.uliandana@gmail.com>
2024-01-24 01:03:52 +0700

    Close icon, stats capitalization, type label color

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,11 +1,41 @@
 'use client'
-import BackIcon from '@/app/.elements/BackIcon';
+import CloseIcon from '@/app/.elements/CloseIcon';
 import Spinner from '@/app/.elements/Spinner';
 import useViewModel from './viewmodel';
 
 type Params = {
   name: string,
 }
+
+const STATS: { [key: string]: string } = {
+  'hp': 'HP',
+  'attack': 'Attack',
+  'defense': 'Defense',
+  'special-attack': 'Special Attack',
+  'special-defense': 'Special Defense',
+  'speed': 'Speed',
+};
+
+const TYPES: { [key: string]: string } = {
+  bug: '#AABB22',
+  dark: '#775544',
+  dragon: '#7766EE',
+  electric: '#FFCC33',
+  fairy: '#FFAAFF',
+  fighting: '#BB5544',
+  fire: '#FF4422',
+  flying: '#6699FF',
+  ghost: '#6666BB',
+  grass: '#77CC55',
+  ground: '#DDBB55',
+  ice: '#77DDFF',
+  normal: '#BBBBAA',
+  poison: '#AA5599',
+  psychic: '#FF5599',
+  rock: '#BBAA66',
+  steel: '#AAAABB',
+  water: '#3399FF',
+};
 
 export default function PokemonByName({ params }: { params: Params }) {
   const { pokemon, berries, isFetching } = useViewModel(params.name);
@@ -21,15 +51,15 @@ export default function PokemonByName({ params }: { params: Params }) {
   return (
     <main className="flex flex-col h-[100vh] overflow-y-auto items-stretch justify-between p-24">
       <header className="flex items-center justify-center">
-        <h1 className="text-[3rem] font-[700] mr-[-2rem] flex-1 text-center">{pokemon?.name}</h1>
-        <a href="/" className="h-[2rem] w-[2rem]">
-          <BackIcon />
+        <h1 className="text-[3rem] font-[700] mr-[-3rem] flex-1 text-center">{pokemon?.name}</h1>
+        <a href="/" className="h-[3rem] w-[3rem] p-[0.5rem] rounded-full bg-red-700">
+          <CloseIcon />
         </a>
       </header>
       <img src={pokemon?.sprite} />
       <div className="flex items-center justify-center gap-[1rem]">
         {pokemon?.types.map(i => (
-          <span className="py-[0.25rem] px-[1rem] text-[1.5rem] uppercase rounded-[0.5rem] font-mono" style={{ backgroundColor: 'grey' }} key={i}>
+          <span className="py-[0.25rem] px-[1rem] text-[1.5rem] text-white uppercase rounded-[0.5rem] font-mono" style={{ backgroundColor: TYPES[i] || 'grey' }} key={i}>
             {i}
           </span>
         ))}
@@ -38,7 +68,7 @@ export default function PokemonByName({ params }: { params: Params }) {
         <tbody>
           {pokemon?.stats.map((i, idx) => (
             <tr key={idx}>
-              <td className="pr-[2rem]">{i.name}</td>
+              <td className="pr-[2rem]">{STATS[i.name]}</td>
               <td>{i.value}</td>
             </tr>
           ))}
@@ -61,8 +91,8 @@ export default function PokemonByName({ params }: { params: Params }) {
           </div>
         </section>
       )}
-      <div className="h-[5rem] overflow-x-auto p-[1rem] rounded-[1rem] border-solid border-[0.125rem] border-white whitespace-nowrap">
-        {berries.map(i => <img key={i.id} src={i.sprite} title={i.name} className="h-[3rem] inline-block" />)}
+      <div className="h-[5rem] overflow-y-hidden overflow-x-auto p-[1rem] rounded-[1rem] border-solid border-[0.125rem] border-white whitespace-nowrap">
+        {berries.map(i => <img key={i.id} src={i.sprite} title={i.name} className="h-[3rem] mx-[1rem] inline-block" />)}
       </div>
       <button className="py-[1rem] rounded-[3rem] text-[2rem] uppercase bg-[green] font-[700] tracking-[0.125rem]">Feed Pokemon</button>
     </main>
