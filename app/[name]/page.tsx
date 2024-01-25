a2edc6c2231e65c0d8edc673cc41449abc9053dc
Oka Uliandana <oka.uliandana@gmail.com>
2024-01-25 21:29:25 +0700

    UI enhancement

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -49,14 +49,14 @@ export default function PokemonByName({ params }: { params: Params }) {
   }
 
   return (
-    <main className="flex flex-col h-[100vh] overflow-y-auto items-stretch justify-between p-24">
+    <main className="flex flex-col gap-[1rem] h-[100vh] overflow-y-auto justify-between p-24">
       <header className="flex items-center justify-center">
         <h1 className="text-[3rem] font-[700] mr-[-3rem] flex-1 text-center">{pokemon?.name}</h1>
         <a href="/" className="h-[3rem] w-[3rem] p-[0.5rem] rounded-full bg-red-700">
           <CloseIcon />
         </a>
       </header>
-      <img src={pokemon?.sprite} />
+      <img className="h-[25vh] self-center" src={pokemon?.sprite} />
       <div className="flex items-center justify-center gap-[1rem]">
         {pokemon?.types.map(i => (
           <span className="py-[0.25rem] px-[1rem] text-[1.5rem] text-white uppercase rounded-[0.5rem] font-mono" style={{ backgroundColor: TYPES[i] || 'grey' }} key={i}>
@@ -74,27 +74,24 @@ export default function PokemonByName({ params }: { params: Params }) {
           ))}
           <tr>
             <td className="pr-[2rem]">Weight</td>
-            <td>{pokemon?.weight} kg</td>
+            <td>{pokemon?.weight}</td>
           </tr>
         </tbody>
       </table>
       {pokemon?.evolvesTo?.length! > 0 && (
-        <section>
-          <p>Evolves to</p>
-          <div className="flex items-center gap-[2rem]">
-            {pokemon?.evolvesTo.map((i, idx) => (
-              <div key={idx}>
-                <img className="w-[5rem]" src={i?.sprite} />
-                <p>{i?.name}</p>
-              </div>
-            ))}
+        <section className="flex gap-[1rem] items-center">
+          <p className="w-[16rem] text-[1.5rem]">Evolves to</p>
+          <div className="h-[5rem] mb-[1rem] overflow-y-hidden overflow-x-auto p-[1rem] whitespace-nowrap">
+            {pokemon?.evolvesTo.map(i => <img key={i?.id} src={i?.sprite} className="h-[4rem] mx-[1rem] inline-block" />)}
           </div>
         </section>
       )}
-      <div className="h-[5rem] overflow-y-hidden overflow-x-auto p-[1rem] rounded-[1rem] border-solid border-[0.125rem] border-white whitespace-nowrap">
-        {berries.map(i => <img key={i.id} src={i.sprite} title={i.name} className="h-[3rem] mx-[1rem] inline-block" />)}
-      </div>
-      <button className="py-[1rem] rounded-[3rem] text-[2rem] uppercase bg-[green] font-[700] tracking-[0.125rem]">Feed Pokemon</button>
+      <footer className="sticky bottom-[-3rem] mb-[-3rem]">
+        <div className="h-[5rem] mb-[1rem] overflow-y-hidden overflow-x-auto p-[1rem] rounded-[1rem] border-solid border-[0.125rem] border-white whitespace-nowrap">
+          {berries.map(i => <img key={i.id} src={i.sprite} title={i.name} className="h-[3rem] mx-[1rem] inline-block" />)}
+        </div>
+        <button className="w-full py-[1rem] rounded-[3rem] text-[2rem] uppercase bg-[green] font-[700] tracking-[0.125rem]">Feed Pokemon</button>
+      </footer>
     </main>
   )
 }
