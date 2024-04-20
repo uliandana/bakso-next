687fd7bd2186bd1ed71410ecca000c889767fe28
Oka Uliandana <oka.uliandana@gmail.com>
2024-04-20 11:16:08 +0700

    Add unit test in utils

---
+++ app/.utils/__tests__/useModal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { describe, expect, jest, test } from '@jest/globals';
+import useModal from '../useModal';
+
+describe('./utils/useModal', () => {
+  test('return modal value & call intercept function', () => {
+    jest.spyOn(React, 'useState');
+    const setModal = jest.fn();
+    (React.useState as jest.Mock).mockImplementation(v => [v, setModal]);
+
+    const [modal, intercept] = useModal('OPEN');
+    expect(modal).toBe('OPEN');
+    intercept('OPEN-2');
+    expect(setModal).lastCalledWith('OPEN-2');
+  });
+
+  test('call intercept function without param (close modal with fading by timeout)', () => {
+    jest.spyOn(React, 'useState');
+    const setModal = jest.fn();
+    (React.useState as jest.Mock).mockImplementation(v => [v, setModal]);
+
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
+
+    const [_, intercept] = useModal('OPEN');
+    intercept('');
+    expect(setModal).lastCalledWith('_FADING_');
+    jest.runAllTimers();
+    expect(setModal).lastCalledWith('');
+  });
+});
